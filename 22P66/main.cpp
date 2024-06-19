1786dbce6d82925341f0ae3bff195b61374c157d
Kirtix1 <166992759+Kirtix1@users.noreply.github.com>
2024-06-19 20:12:02 +0200

    Add files via upload

---
+++ 22P66/main.cpp
@@ -0,0 +1,12 @@
+//st_cale.c -- zamienia stopy na cale
+#include <stdio.h>
+
+int main(void)
+{
+    int stopy, cale;
+
+    stopy = 2;
+    cale = 12 * stopy;
+    printf("%d stopy równają się %d calom!\n", stopy, cale);
+    return 0;
+}
